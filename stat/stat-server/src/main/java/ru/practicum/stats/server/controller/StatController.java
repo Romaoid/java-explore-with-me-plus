57a8f42e10afa1b71b291e1114817d12847fcaed
Romaoid <Romaoid@yandex.ru>
2026-05-26 16:17:17 +0300

    2. add functional -1. реализовать эндпоинты в контроллере; -2. добавить сущность Stat для записи в бд и вспомогательную сущность App; -3. реализовать JpaRepository для Stat и App; -4. добавить представление ViewStat и JPQL запрос для получения списка статистики; -5. добавить методы для записи и выдачи статистики в сервисном классе.

--- stat/stat-server/src/main/java/ru/practicum/stats/server/controller/StatController.java
+++ stat/stat-server/src/main/java/ru/practicum/stats/server/controller/StatController.java
@@ -1,35 +1,40 @@
 package ru.practicum.stats.server.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stats.dto.EndpointHitDto;
 import ru.practicum.stats.dto.ViewStatsDto;
-import ru.practicum.stats.server.model.ReqParam;
 import ru.practicum.stats.server.service.StatService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class StatController {
     private final StatService service;
 
     @PostMapping(path = "/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addStat(@RequestBody EndpointHitDto dto) {
-        //satatuscode201
-        //add logging
-        //add error-handler
+        log.info("Вызван эндпоинт добавления запроса с данными: {}", dto);
         service.addStat(dto);
     }
 
     @GetMapping(path = "/stats")
-    public List<ViewStatsDto> getStats(@Valid ReqParam params) {
-            //@RequestParam String start,
-//                         @RequestParam String end,
-//                         @RequestParam(defaultValue = "false", required = false) boolean unique,
-//                         @RequestParam(defaultValue = "", required = false) List<String> uris) {
+    public List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                       @RequestParam(defaultValue = "false") boolean unique,
+                                       @RequestParam(required = false) List<String> uris) {
+        log.info("Вызван эндпоинт на получение списка статистики с параметрами: start = {},\nend = {},\nunique = {},\nuris = {}",
+                start, end, unique, uris);
 
-        return service.getStats(params);
+        uris = (uris == null || uris.isEmpty()) ? uris : null;
+
+        return service.getStats(start, end, unique, uris);
     }
 }
