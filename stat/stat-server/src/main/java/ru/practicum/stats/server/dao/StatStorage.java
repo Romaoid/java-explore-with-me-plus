8c3a93cce23cb7a9da749733ad96aa195a400b4d
Romaoid <Romaoid@yandex.ru>
2026-05-26 16:17:17 +0300

    2. add functional -1. реализовать эндпоинты в контроллере; -2. добавить сущность Stat для записи в бд и вспомогательную сущность App; -3. реализовать JpaRepository для Stat и App; -4. добавить представление ViewStat и JPQL запрос для получения списка статистики; -5. добавить методы для записи и выдачи статистики в сервисном классе.

--- stat/stat-server/src/main/java/ru/practicum/stats/server/dao/StatStorage.java
+++ stat/stat-server/src/main/java/ru/practicum/stats/server/dao/StatStorage.java
@@ -3,22 +3,22 @@ package ru.practicum.stats.server.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.stats.dto.ViewStatsDto;
 import ru.practicum.stats.server.model.Stat;
+import ru.practicum.stats.server.model.ViewStat;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatStorage extends JpaRepository<Stat, Long> {
 
-    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(s.app.name, s.uri, " +
-            "CASE WHEN :unique = true THEN COUNT(DISTINCT s.ip) ELSE COUNT(s.id) END) " +
+    @Query("SELECT s.app.name AS appName, s.uri AS uri, " +
+            "CASE WHEN :unique = true THEN COUNT(DISTINCT s.ip) ELSE COUNT(s.id) END AS hits " +
             "FROM Stat s " +
             "WHERE s.timestamp BETWEEN :start AND :end " +
             "AND (:uris IS NULL OR s.uri IN :uris) " +
-            "GROUP BY s.app.name, s.uri ")
-    List<ViewStatsDto> getStats(@Param("start") LocalDateTime start,
-                                @Param("end") LocalDateTime end,
-                                @Param("unique") boolean unique,
-                                @Param("uris") List<String> uris);
+            "GROUP BY s.app.name, s.uri")
+    List<ViewStat> getStats(@Param("start") LocalDateTime start,
+                            @Param("end") LocalDateTime end,
+                            @Param("unique") boolean unique,
+                            @Param("uris") List<String> uris);
 }
