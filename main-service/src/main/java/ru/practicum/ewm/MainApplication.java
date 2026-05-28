1a19ad26e4aa76028c4a73d405b9598e48240715
Romaoid <Romaoid@yandex.ru>
2026-05-28 22:34:42 +0300

    hotfix Убрал код от тестирования

--- main-service/src/main/java/ru/practicum/ewm/MainApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -3,24 +3,10 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import ru.practicum.stats.client.StatClient;
-import ru.practicum.stats.dto.ViewStatsDto;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
-@ComponentScan(value = {"ru.practicum.ewm", "ru.practicum.stats"})
 public class MainApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
-
-        StatClient client = context.getBean(StatClient.class);
-        client.hit("integrationTest", "/test", "192.168.1.1", LocalDateTime.now());
-        List<ViewStatsDto> dtos = client.getStat(
-                LocalDateTime.now().minusDays(1),
-                LocalDateTime.now().plusDays(1), null, null);
-        System.out.println(dtos);
     }
 }
