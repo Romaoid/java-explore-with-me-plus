86776c63636789463ba2627771b1b1c77b71da41
Clulnes <art.yusupov2003@yandex.ru>
2026-06-01 21:08:22 +0300

    Готовая логика для подборки событий(как для админа, так и для обычного юзера)
    # Conflicts:
    #	main-service/src/main/java/ru/practicum/ewm/dao/EventRepository.java
    #	main-service/src/main/java/ru/practicum/ewm/mapper/EventMapper.java
    #	main-service/src/main/java/ru/practicum/ewm/model/Event.java
    #	main-service/src/main/java/ru/practicum/ewm/model/Location.java

---
+++ main-service/src/main/java/ru/practicum/ewm/mapper/CompilationMapper.java
@@ -0,0 +1,19 @@
+package ru.practicum.ewm.mapper;
+
+import ru.practicum.ewm.dto.CompilationDto;
+import ru.practicum.ewm.model.Compilation;
+
+import java.util.stream.Collectors;
+
+public class CompilationMapper {
+    public static CompilationDto toCompilationDto(Compilation compilation) {
+        return CompilationDto.builder()
+                .id(compilation.getId())
+                .title(compilation.getTitle())
+                .pinned(compilation.getPinned())
+                .events(compilation.getEvents().stream()
+                        .map(EventMapper::toEventDto)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
